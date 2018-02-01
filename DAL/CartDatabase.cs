9b08d20dc0b1443203894c07573189f210f13cfd
Nathan <nate@11rax.com>
2018-02-01 14:11:34 +1100

    Cart Database singleton approach

--- DAL/CartDatabase.cs
+++ DAL/CartDatabase.cs
@@ -1,4 +1,5 @@
-﻿using ShoppingCart.Models;
+﻿using ShoppingCart.DAL;
+using ShoppingCart.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,19 +7,47 @@ using System.Web;
 
 namespace ShoppingCart.DAL
 {
-
-    /**
-     *   A singleton to represent the cart. 
-     *   As per the specification, can be stored in memory. 
-     *   No need for data persistence.   
-     */
+    /// <summary>
+    /// Class used to maintain the user's purchases. 
+    /// Assumption: The specification does not require (1) user accounts and (2) a method to manage multiple carts.
+    /// Could have used Sessions object to persist a given users cart as follows:
+    /// public static CartDatabase Instance
+    /// {
+    ///     get
+    ///     {
+    ///         var cart = HttpContext.Current.Session["Cart"] as CartDatabase;
+    ///         if (null == cart)
+    ///         {
+    ///             cart = new CartDatabase();
+    ///             HttpContext.Current.Session["Cart"] = cart;
+    ///         }
+    ///         return cart;
+    ///     }
+    /// }
+    ///
+    /// However by the server preserving sessions, this would no longer be RESTful as it violates the `Stateless` constraint.
+    /// </summary>
     public class CartDatabase
     {
-
         private ProductDatabase productDb;
         private List<OrderItem> currentBasket;
 
-        public CartDatabase()
+
+        private static CartDatabase instance;
+        public static CartDatabase Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new CartDatabase();
+                }
+                return instance;
+            }
+        }
+
+
+        private CartDatabase()
         {
             productDb = new ProductDatabase();
             currentBasket = new List<OrderItem>();
@@ -30,10 +59,12 @@ namespace ShoppingCart.DAL
             return currentBasket;          
         }
 
+
         public OrderItem Get(int id)
         {
             return currentBasket.Where(p => p.Id == id).FirstOrDefault();
         }
+
 
         public bool UpdateQuantity(int id, int qty)
         {
@@ -48,6 +79,7 @@ namespace ShoppingCart.DAL
             return false;
         }
 
+
         public bool Add(int id, int qty)
         {
             Product prod = productDb.Get(id);
@@ -58,8 +90,23 @@ namespace ShoppingCart.DAL
             {
                 item.OrderQty = item.OrderQty += qty;
             }
+            else
+            {
+                currentBasket.Add(new OrderItem(prod, qty));
+            }
+            return true;
+        }
 
-            currentBasket.Add(new OrderItem(prod, qty));
+
+        public bool Remove(int id)
+        {
+            OrderItem itemToRemove = currentBasket.Find(p => p.Id == id);
+            return currentBasket.Remove(itemToRemove);
+        }
+
+        public bool EmptyCart()
+        {
+            currentBasket = new List<OrderItem>();
             return true;
         }
 
