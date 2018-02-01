b49abef95750c7fe13604edb393c46345b0b7746
Nathan <nate@11rax.com>
2018-02-02 10:03:08 +1100

    update to js helper lib

--- Scripts/BasketHelper.js
+++ Scripts/BasketHelper.js
@@ -2,35 +2,51 @@
     Helper script for ShoppingCart end points.
 */
 
-function AppendToDebug(caller, status, result) {
-    var oldText = $("#debug").text();
-    var newText = oldText + caller + "[" + status + "]<br/>" + result + "<br/><br/>";
-    $("#debug").text(newText);
-}
-
 function ShoppingCartHelper() {
 
     this.get = function (id) {
         $.get("/api/Basket/" + id), function (data, status) {
-            AppendToDebug("get", status, data);
+            var funcName = arguments.callee.toString();
+            console.log(funcName + ": " + data + "[" + status + "]");
         }
     }
 
     this.getAll = function () {
         $.get("/api/Basket"), function (data, status) {
-            AppendToDebug("getAll", status, data);
+            var funcName = arguments.callee.toString();
+            console.log(funcName + ": " + data + "[" + status + "]");
         }
     }
 
     this.getBasketTotal = function () {
         $.get("/api/Basket/total"), function (data, status) {
-            AppendToDebug("getBasketTotal", status, data);
+            var funcName = arguments.callee.toString();
+            console.log(funcName + ": " + data + "[" + status + "]");
         }
     }
 
     this.AddProductToCart = function (id, qty) {
-        $.post("/api/Basket", { Id: id, OrderQty: qty }, function (result) {
-            AppendToDebug("AddProductToCart", status, result);
+        $.post("/api/Basket",
+            {
+                Id: id,
+                OrderQty: qty
+            },
+            function (result) {
+                var status = "no_status";
+                var funcName = arguments.callee.toString();
+                console.log(funcName + ": " + data + "[" + status + "]");
+            });
+    }
+
+    this.UpdateQtyOfProduct = function (id, qty) {
+        $.ajax({
+            url: '/api/Basket',
+            type: 'PUT',
+            success: function (result) {
+                var status = "no_status";
+                var funcName = arguments.callee.toString();
+                console.log(funcName + ": " + data + "[" + status + "]");
+            }
         });
     }
 }
